8ed1dc05c696b40c5fe38867fec12c10ed80fb9c
Wuyika <wuyika.serge@gmail.com>
2024-05-22 19:24:55 +0100

    fonts

---
+++ lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFF1F2A40);
+  static const secondary = Color(0xFF6E6EF7);
+  static const backGroundColor = Color(0xFFF1F2FA);
+  static const inactive = Color(0xFFC9C9C9);
+
+
+  static const warning = Color(0xFFF96868);
+  static const textColor = Color(0xFF404040);
+
+}
